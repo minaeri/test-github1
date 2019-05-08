a129d0c2853b80e8fd260284b1249c51736808ec
minaeri <aeri.min.1003@gmail.com>
2019-05-08 16:30:57 +0900

    Add sum function

--- Calculator.java
+++ Calculator.java
@@ -2,7 +2,14 @@ package com.mycompany.test;
 
 public class Calculator {
   public static void main(String[] args){
-
+    int a = 4;
+    int b = 2;
+    System.out.println(sum(a,b));
   }
 
+}
+
+
+priate static int sum(int a, int b){
+    return a+b;
 }
